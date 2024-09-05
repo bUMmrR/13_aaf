4e180289e1622f33a6f04bb9c57ffa2d39d6f725
Agócs Dániel <agocs.daniel@ckik.loc>
2024-09-05 11:39:06 +0200

    asd

--- aknakereső/aknakereső/Program.cs
+++ aknakereső/aknakereső/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,18 @@ namespace aknakereső
         static List<List<char>> t = new List<List<char>>(); 
         static void Main(string[] args)
         {
-            tombFeltoltes(6, 10);
+            tombFeltoltes(6, 10, 10);
             tombKiiras();
+            korBekeres();
+
             Console.ReadLine();
+        }
+
+        private static void korBekeres()
+        {
+            Console.WriteLine("Add meg a a helyed (x,y kordináta szóközzel elválasztva)");
+            string input = Console.ReadLine();
+            int x = input.Split('');
         }
 
         private static void tombKiiras()
@@ -22,13 +32,13 @@ namespace aknakereső
             {
                 for (int j = 0; j < t[0].Count; j++)
                 {
-                    Console.Write(t[i][j]);
+                    Console.Write(t[i][j] +"\t");
                 }
                 Console.WriteLine();
             }
         }
 
-        private static void tombFeltoltes(int length, int height)
+        private static void tombFeltoltes(int length, int height, int chance)
         {
             Random rnd = new Random();
             for (int i = 0; i < length; i++)
@@ -36,7 +46,7 @@ namespace aknakereső
                 List<char> segedList = new List<char>();
                 for (int j = 0; j < height; j++)
                 {
-                    if (rnd.Next(0,4) == 1)
+                    if (rnd.Next(0,chance) == 1)
                     {
                         segedList.Add('x');
                     }
